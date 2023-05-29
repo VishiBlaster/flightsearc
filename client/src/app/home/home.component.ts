ad7d169457e473236d364ea79faad7e13e91279c
VishiBlaster <mongajatin2588@gmail.com>
2023-05-29 15:21:20 +0530

    made home page

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  searchForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.searchForm = this.formBuilder.group({
+      origin: ['', Validators.required],
+      destination: ['', Validators.required],
+      departureDate: ['', Validators.required],
+      returnDate: [''],
+      passengerCount: ['1', Validators.required]
+    });
+  }
+
+  ngOnInit() {}
+
+  onSubmit() {
+    if (this.searchForm.invalid) {
+      return;
+    }
+
+    // Perform search logic
+    console.log('Search form submitted:', this.searchForm.value);
+  }
 }
